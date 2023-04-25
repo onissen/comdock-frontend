59c60a0fbefa7fae0bc763add9ac7d7a80e4d467
Oke <56229919+onissen@users.noreply.github.com>
2023-04-25 20:23:02 +0200

    Typo Basics

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,15 @@ export default function Index() {
   return (
     <>
       <Layout>
-        Index
+        <div className="m-5">
+          <h1>Überschrift 1</h1>
+          <h2>Überschrift 2</h2>
+          <h3>Überschrift 3</h3>
+          <h4 >Überschrift 4</h4>
+          <h5>Überschrift 5</h5>
+          <h6>Überschrift 6</h6>
+        </div>
+
       </Layout>
     </>
   );
