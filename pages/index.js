18032e93cc4f27ef89a0dce1115d68d82539dc2e
Oke <56229919+onissen@users.noreply.github.com>
2023-04-28 16:15:51 +0200

    Removed Typo Preview

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,7 @@ export default function Index() {
   return (
     <>
       <Layout>
-        <div className="m-5">
-          <h1>Überschrift 1</h1>
-          <h2>Überschrift 2</h2>
-          <h3>Überschrift 3</h3>
-          <h4 >Überschrift 4</h4>
-          <h5>Überschrift 5</h5>
-          <h6>Überschrift 6</h6>
-        </div>
-
+        <h1>Index Page</h1>
       </Layout>
     </>
   );
