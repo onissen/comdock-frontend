77fca9915f1fb53d6f1fa0276eae632f7349cb19
Oke <56229919+onissen@users.noreply.github.com>
2023-05-01 13:34:11 +0200

    Added furtherNames and branches Informations; Added fallback loops for empty data statements

--- pages/companies/[pageslug].js
+++ pages/companies/[pageslug].js
@@ -1,15 +1,33 @@
 import Layout from "@/components/common/Layout"
 import DetailPage from "@/components/pagetypes/DetailPage";
 import { fetcher } from "@/helpers/api";
+import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CompanyDetail = ({item}) => {
     return(
         <Layout siteTitle={item.attributes.company_name}>
-            <DetailPage title={item.attributes.company_name+', '+item.attributes.main_branch.data.attributes.city} contentType='company'>
+            <DetailPage 
+                title={item.attributes.main_branch && item.attributes.main_branch.data ? 
+                    item.attributes.company_name + ', ' + item.attributes.main_branch.data.attributes.city :
+                    item.attributes.company_name
+                }
+                contentType='company'>
                 <section id="company_name" className="detailSection">
                     <h4 className="sectionLabel">Firma</h4>
                     <p>{item.attributes.company_name}</p>
-                    <p>former_names</p>
+                    <p id="furtherNames">
+                        {item.attributes.furtherNames &&
+                            item.attributes.furtherNames.map((furtherName) => {
+                                return (
+                                    <p className="flex text-primary-400/50" key={furtherName.id}>
+                                        <FontAwesomeIcon icon={faArrowRightArrowLeft} className="w-3 flex-none mr-2" />
+                                        <span className="flex-auto text-sm">{furtherName.further_cname}</span>
+                                    </p>
+                                )
+                            })
+                        } 
+                    </p>
                 </section>
                 <section id="register" className="detailSection">
                     <h4 className="sectionLabel">Register</h4>
@@ -24,21 +42,36 @@ const CompanyDetail = ({item}) => {
                         </p>
                     ) : ''}
                 </section>
-                <section id="branches" className="detailSection">
-                    <h4 className="sectionLabel">Sitz, Niederlassung, Zweigniederlassungen</h4>
-                    <p id="main_branch">
-                        <span className="badge">Sitz</span>
-                        <span>
-                            {item.attributes.main_branch.data.attributes.street} {item.attributes.main_branch.data.attributes.place_number}, 
-                            {item.attributes.main_branch.data.attributes.zip} {item.attributes.main_branch.data.attributes.city}
-                        </span>
-                    </p>
-                    <p>branches</p>
-                </section>
-                <section id="corp_object" className="detailSection">
-                    <h4 className="sectionLabel">Unternehmensgegenstand</h4>
-                    <p>{item.attributes.corp_object}</p>
-                </section>
+                {item.attributes.main_branch && item.attributes.main_branch.data ||
+                    item.attributes.branches && item.attributes.branches.data.length > 0 ? (
+                    <section id="addresses" className="detailSection">
+                        <h4 className="sectionLabel">Sitz, Niederlassung, Zweigniederlassungen</h4>
+                        {item.attributes.main_branch && item.attributes.main_branch.data ? (
+                        <p id="main_branch">
+                            <span className="badge">Sitz</span>
+                            <span>
+                                {item.attributes.main_branch.data.attributes.street} {item.attributes.main_branch.data.attributes.place_number}, {item.attributes.main_branch.data.attributes.zip} {item.attributes.main_branch.data.attributes.city}
+                            </span>
+                        </p>
+                        ) : ''}
+                        <div id="branches">
+                            {item.attributes.branches && item.attributes.branches.data.map((branch) => (
+                                    <p key={branch.id}>
+                                        <span className="badge">Zweigniederlassung</span>
+                                        <span>
+                                            {branch.attributes.street} {branch.attributes.place_number}, {branch.attributes.zip} {branch.attributes.city}
+                                        </span>
+                                    </p>
+                            ))}
+                        </div>
+                    </section>
+                ) : ('')}
+                {item.attributes.corp_object ? (
+                    <section id="corp_object" className="detailSection">
+                        <h4 className="sectionLabel">Unternehmensgegenstand</h4>
+                        <p>{item.attributes.corp_object}</p>
+                    </section>
+                ) : '' }
                 <section id="network" className="detailSection">
                     <h4 className="sectionLabel">Netzwerk</h4>
                     Netzwerk CardList
@@ -52,7 +85,7 @@ const CompanyDetail = ({item}) => {
 export async function getServerSideProps({params}) {
     const {pageslug} = params;
     const contentResponse = await fetcher(
-        `${process.env.NEXT_PUBLIC_STRAPI_URL}/slugify/slugs/company/${pageslug}?populate=*`
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/slugify/slugs/company/${pageslug}?populate=*&_sort=furtherNames.name_upto:ASC`
     )
     return{
         props: {
