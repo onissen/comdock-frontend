0307ae088333ca26562fd805af68e850e662beae
Oke <56229919+onissen@users.noreply.github.com>
2023-07-13 20:16:44 +0200

    Some kind of upload works 413 is a problem using strapi local upload from docs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
 import Layout from "@/components/common/Layout";
+import { fetcher } from "@/helpers/helpScripts";
+import { useEffect } from "react";
 export default function Index() {
+
+  if (typeof window !== "undefined") {      
+    const form = document.querySelector('form');
+    form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      await fetcher(
+        'upload',
+        '',
+        {
+          method: 'post',
+          body: new FormData(e.target)
+        }
+      )
+    })
+  }
+
   return (
     <Layout nopageHeader>
-      <h1>Index Page</h1>
+      <form>
+        <input type="file" name="files" />
+        <input type="text" name="ref" value="api::cert-document.cert-document" />
+        <input type="text" name="refId" value="3" />
+        <input type="text" name="field" value="document" />
+        <input type="submit" value="Submit" />
+      </form>
     </Layout>
   );
 }
